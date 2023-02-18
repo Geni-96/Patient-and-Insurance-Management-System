3f85a4d6f0efb9e66dfa90a93d513395a1783870
Rohan Shah <rohanshah@Rohans-MacBook-Air-4.local>
2023-02-18 11:40:37 -0500

    initial setup

--- PNIM/main/views.py
+++ PNIM/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+def index(response):
+    print("reach view folder")
+    return render(response, "main/base.html",{})
+
+def register(response):
+    form = UserCreationForm()
+    return render(response, "main/register.html",{"form":form})
+
+def login(response):
+    return render(response, "main/login.html",{})

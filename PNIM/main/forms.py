803bed96c41134cd1937e56024d3160240ac4a22
Rohan Shah <rohanshah@Rohans-MacBook-Air-4.local>
2023-02-18 18:49:43 -0500

    Login/Signup using postgres

--- PNIM/main/forms.py
+++ PNIM/main/forms.py
@@ -1,17 +1,17 @@
 from django import forms
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
-    user_type =(
-        ("1", "Patient"),
-        ("2", "Doctor"),
-        ("3", "Insurance Provider"),
-    )
-    email = forms.EmailField()
-    type = forms.ChoiceField(choices = user_type)
+types =(
+    ("1", "Patient"),
+    ("2", "Doctor"),
+    ("3", "Insurance"),
+)
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    user_type = forms.ChoiceField(choices = types, required=True)
 
     class Meta:
         model = User
-        fields = ["username","email","type","password1","password2"]
+        fields = ["username","email","user_type","password1","password2"]

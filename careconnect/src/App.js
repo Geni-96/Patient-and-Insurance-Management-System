443da6a8051b6635ab999f1bdef4a19ca55225fb
Cole Downard, Dedra Ann <dcoledow@iu.edu>
2023-03-01 22:10:13 -0500

    Update App.js

--- careconnect/src/App.js
+++ careconnect/src/App.js
@@ -2,12 +2,19 @@
 import './App.css';
 // import Login from "./LoginPage/Login"
 import Registration from "./RegistrationPage/Registration"
+import Pwd from "./password/Pwd"
+// import RecoveryLink from "./password/RecoveryLink"
+// import Reset from "./password/Reset"
 
 
 function App() {
   return (
     <div className="pages">
       <Registration />
+        {/* <Pwd /> */}
+        {/* <Login /> */}
+        {/* <RecoveryLink /> */}
+        {/* <Reset /> */}
     </div>
   );
 }

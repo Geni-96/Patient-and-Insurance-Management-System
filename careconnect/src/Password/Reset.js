db53ed8428ad1a238c5351a023f3b6aef6346255
Cole Downard, Dedra Ann <dcoledow@iu.edu>
2023-03-01 22:22:49 -0500

    Create Reset.js

---
+++ careconnect/src/Password/Reset.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+// import Login from "./LoginPage/Login"
+// import Registration from './RegistrationPage/Registration'
+// import Pwd from "./Password/Pwd"
+// import RecoveryLink from "./Password/RecoveryLink"
+import Reset from "./Password/Reset"
+
+function App() {
+  return (
+    <div className="App">
+            {/* <Registration /> */}
+        {/* <Pwd /> */}
+        {/* <Login /> */}
+        {/* <RecoveryLink /> */}
+        <Reset />
+    </div>
+  );
+}
+
+export default App;

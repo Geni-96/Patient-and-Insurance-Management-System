76cf0fcf9cf10d0fc7023f1b8765ec1e250176d5
Gnaneswari <gnlolugu@iu.edu>
2023-03-26 15:30:54 -0400

    "package changes"

--- careconnect/src/App.js
+++ careconnect/src/App.js
@@ -1,21 +1,25 @@
 
 import './App.css';
-// import Login from "./LoginPage/Login"
 import Registration from "./RegistrationPage/Registration"
-import Pwd from "./password/Pwd"
+// import Pwd from "./password/Pwd"
 // import RecoveryLink from "./password/RecoveryLink"
 // import Reset from "./password/Reset"
-
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from "./LoginPage/Login"
+// import LandingPage from "./LoginPage/LandingPage"
+import Mfa from './MFA/Mfa';
 
 function App() {
   return (
-    <div className="pages">
-      <Registration />
-        {/* <Pwd /> */}
-        {/* <Login /> */}
-        {/* <RecoveryLink /> */}
-        {/* <Reset /> */}
-    </div>
+    <BrowserRouter>
+    <Routes>
+      {/* <Route path="/" element={<LandingPage />}></Route> */}
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/registration" element={<Registration />}></Route>
+      <Route path="/mfa" element={<Mfa />}></Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 

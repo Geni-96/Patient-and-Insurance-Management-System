e0626d5b17c088acbc2dfa37e422a7fac51c68ea
Cole Downard, Dedra Ann <dcoledow@iu.edu>
2023-03-26 01:04:55 -0400

    updated Reset.js

--- careconnect/src/Password/Reset.js
+++ careconnect/src/Password/Reset.js
@@ -1,21 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-// import Login from "./LoginPage/Login"
-// import Registration from './RegistrationPage/Registration'
-// import Pwd from "./Password/Pwd"
-// import RecoveryLink from "./Password/RecoveryLink"
-import Reset from "./Password/Reset"
+import React from 'react';
+import './reset.css';
+import CareConnect from '../LoginPage/CareConnect.png'
 
-function App() {
-  return (
-    <div className="App">
-            {/* <Registration /> */}
-        {/* <Pwd /> */}
-        {/* <Login /> */}
-        {/* <RecoveryLink /> */}
-        <Reset />
-    </div>
-  );
+export default function Reset(){
+    return(
+        <>
+            <div className="card">
+            <img className="logo" src={CareConnect} alt="logo"></img>
+                <div className="innerCard">
+                    <p className="lock-icon"><i className="fas fa-lock"></i></p>
+                    <h2>Change Password</h2>
+                    <p>To protect your account, create a password that:</p>
+                    <ul>
+                        <li>Has at least 10 characters</li>
+                        <li className="list">Contains at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character</li>
+                        <li>Does not contain any part of your name</li>
+                        <li>Is not one of your previously used passwords</li>
+                    </ul>
+                    <p className="passwd">New Password</p>
+                    <input type="password" className="passInput"></input>
+                    <p className="passwd">Re-enter Your New Password</p>
+                    <input type="password" className="passInput"></input>
+                    <button>Change My Password</button>
+                </div>
+            </div>
+        </>)
 }
-
-export default App;

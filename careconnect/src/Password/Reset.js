b0de417a273e99c4611dca9f77a1e56cdfd19bfb
Cole Downard, Dedra Ann <dcoledow@iu.edu>
2023-04-07 00:32:32 -0400

    Update to html

--- careconnect/src/Password/Reset.js
+++ careconnect/src/Password/Reset.js
@@ -1,28 +1,64 @@
-import React from 'react';
-import './reset.css';
-import CareConnect from '../LoginPage/CareConnect.png'
-
-export default function Reset(){
-    return(
-        <>
-            <div className="card">
-            <img className="logo" src={CareConnect} alt="logo"></img>
-                <div className="innerCard">
-                    <p className="lock-icon"><i className="fas fa-lock"></i></p>
-                    <h2>Change Password</h2>
-                    <p>To protect your account, create a password that:</p>
-                    <ul>
-                        <li>Has at least 10 characters</li>
-                        <li className="list">Contains at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character</li>
-                        <li>Does not contain any part of your name</li>
-                        <li>Is not one of your previously used passwords</li>
-                    </ul>
-                    <p className="passwd">New Password</p>
-                    <input type="password" className="passInput"></input>
-                    <p className="passwd">Re-enter Your New Password</p>
-                    <input type="password" className="passInput"></input>
-                    <button>Change My Password</button>
-                </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reset.css" />
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+    <title>Reset</title>
+</head>
+<body>
+    <header id="header" class="fixed-top d-flex align-items-center ">
+        <div class="container d-flex justify-content-between align-items-center">
+            <nav class="nav">
+                <img src='react-logo.png' class="nav-logo" />
+                <ul class="nav-items-2">
+                <li><a href="www.google.com"> Home</a></li>
+                <li><a href="www.google.com">About Us</a></li>
+                <li><a href="www.google.com">Services</a></li>
+                <ul class="nav-items">  
+                <!-- <li><button class="btn btn-primary" ><a class="dif" href="/signup">Sign Up</a></button></li>
+                <li><button class="btn btn-primary"><a class="dif" href="/login">Log In </a></button></li> 
+                </ul>
+                </ul> -->
+            </nav> 
+        </div>
+      </header>
+    <div class="outer-card">
+    <div class="card"> 
+    <!-- <img class="logo" src="CareConnect.png" alt="logo"></img> -->
+        <div class="innerCard">
+            <!-- <p class="lock-icon"><i class="fas fa-lock"></i></p> -->
+            <h2>Change Password</h2>
+            <p>To protect your account, create a password that:</p>
+            <ul>
+                <li>Has at least 10 characters</li>
+                <li class="list">Contains at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character</li>
+                <li>Does not contain any part of your name</li>
+                <li>Is not one of your previously used passwords</li>
+            </ul>
+            <p class="passwd">New Password</p>
+            <input type="password" class="passInput"></input>
+            <p class="passwd">Re-enter Your New Password</p>
+            <input type="password" class="passInput"></input>
+            <button>Change My Password</button>
+        </div>
+    </div>
+    </div>
+    <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-12 text-center">
+              <p>&copy; 2023 CareConnect Group 23. All rights reserved.</p>
+              <ul class="social-icons">
+                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+              </ul>
             </div>
-        </>)
-}
+          </div>
+        </div>
+    </footer>
+</body>
+</html>

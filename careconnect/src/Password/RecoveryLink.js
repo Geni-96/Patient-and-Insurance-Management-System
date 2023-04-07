789e37cb42a82861bdaaaaa0187e5e59f4ab3d09
Cole Downard, Dedra Ann <dcoledow@iu.edu>
2023-04-07 00:31:05 -0400

    Update to html

--- careconnect/src/Password/RecoveryLink.js
+++ careconnect/src/Password/RecoveryLink.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import './recoveryLink.css';
-
-export default function RecoveryLink(){
-    return(
-        <>
-            <div class="card">
-                <p class="lock-icon"><i class="fas fa-lock"></i></p>
-                <h2>Password Reset</h2>
-                <p>An email has been sent.</p><p> Please click the link in the email to reset your password.</p>
-            </div>
-        </>)
-}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="recoveryLink.css" />
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+    <title>recoveryLink</title>
+  </head>
+  <body>
+    <header id="header" class="fixed-top d-flex align-items-center ">
+      <div class="container d-flex justify-content-between align-items-center">
+          <nav class="nav">
+              <img src='react-logo.png' class="nav-logo" />
+              <ul class="nav-items-2">
+              <li><a href="www.google.com"> Home</a></li>
+              <li><a href="www.google.com">About Us</a></li>
+              <li><a href="www.google.com">Services</a></li>
+              <ul class="nav-items">  
+              <!-- <li><button class="btn btn-primary" ><a class="dif" href="/signup">Sign Up</a></button></li>
+              <li><button class="btn btn-primary"><a class="dif" href="/login">Log In </a></button></li> 
+              </ul>
+              </ul> -->
+          </nav> 
+      </div>
+    </header>
+    <div class="outer-card">
+      <div class="card">
+        <p class="lock-icon"><i class="fas fa-lock"></i></p>
+        <h2>Password Reset</h2>
+        <p>An email has been sent.</p>
+        <p>Please click the link in the email to reset your password.</p>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <p>&copy; 2023 CareConnect Group 23. All rights reserved.</p>
+            <ul class="social-icons">
+              <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
